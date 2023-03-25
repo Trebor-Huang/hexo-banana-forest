d518e5f37e5102a04fcf37c6979ebd3342df3c00
Trebor-Huang <2300936257@qq.com>
2023-03-25 22:56:32 +0800

    Grafting?

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
 import { rawWork, globalContext } from 'btex';
 import Hexo from 'hexo';
+import { JSDOM } from 'jsdom';
+import path from 'path';
 
-async function renderBtx(this: Hexo, data: Hexo.extend.RendererData){
-    const result = await rawWork({
+const promises : Record<string, Promise<string>> = {};
+
+async function _renderBtx(hexo: Hexo, data: Hexo.extend.RendererData){
+    const data_path = data.path ?? "";
+    // Render btex
+    // todo: prepare my own prelude
+    const {html, errors, warnings} = await rawWork({
         code: data.text,
         globalContext
     });
-    return result.html;
+    const { window } = new JSDOM(html);
+    // Setup links and transclusions
+    const grafts = window.document.getElementsByTagName("btex-fun");
+    for (const graft of grafts) {
+        // todo: {{image| ... }} are images
+        const graft_name = graft.getAttribute("data-name");
+        if (graft_name === null) {
+            continue;  // todo
+        }
+        const graft_path = path.join(data_path, "..", graft_name + ".btx");
+        console.log("Banana wants: ", graft_path);
+        if (! (graft_path in promises)) {
+            // The graft does not exist
+            graft.outerHTML = `<u>${graft_name}</u>`;
+        } else {
+            graft.outerHTML = await promises[graft_path];
+        }
+    }
+    // Output
+    return window.document.documentElement.outerHTML;
 }
-renderBtx.disableNunjucks = true
+
+function renderBtx(this: Hexo, data: Hexo.extend.RendererData){
+    const promise = _renderBtx(this, data);
+    if (data.path) {
+        promises[data.path] = promise;
+        console.log("Banana has: ", data.path);
+    }
+    return promise;
+}
+renderBtx.disableNunjucks = true;
 
 hexo.extend.renderer.register('btx', 'html', renderBtx);
