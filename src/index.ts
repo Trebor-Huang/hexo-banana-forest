2b0d16cd92b7cfa792b071b71bb159061e08064c
Trebor-Huang <2300936257@qq.com>
2023-03-26 14:00:10 +0800

    Links

--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,20 @@ async function _renderBtx(hexo: Hexo, data: Hexo.extend.RendererData){
     const { window } = new JSDOM(html);
     // Setup links and transclusions
     const grafts = window.document.getElementsByTagName("btex-fun");
-    for (const graft of grafts) {
+    for (const graft of [...grafts]) {
         // todo: {{image| ... }} are images
         const graft_name = graft.getAttribute("data-name");
         if (graft_name === null) {
             continue;  // todo
         }
         const graft_path = path.join(data_path, "..", graft_name + ".btx");
-        console.log("Banana wants: ", graft_path);
+        // console.log("Banana wants: ", graft_path);
         if (! (graft_path in promises)) {
             // The graft does not exist
             graft.outerHTML = `<u>${graft_name}</u>`;
         } else {
             const tree = hexo.theme.getView('partials/tree')
-                ?? hexo.theme.getView('index');
+                ?? hexo.theme.getView('index');  // TODO outside this?
             if (tree === undefined) {
                 throw new Error("Banana: No good layout found.");
             }
@@ -44,8 +44,18 @@ async function _renderBtx(hexo: Hexo, data: Hexo.extend.RendererData){
     }
 
     const links = window.document.getElementsByTagName("btex-link");
-    // see if it is external, and see if it exists
-
+    for (const link of [...links]) {
+        const ref = link.getAttribute("data-page") ?? "";
+        const ref_path = path.join(data_path, "..", ref + ".btx");
+        if (ref_path in promises) {
+            const real_link = document.createElement('a');
+            real_link.href = ref;  // TODO correct linking
+            real_link.innerHTML = link.innerHTML;
+            link.outerHTML = real_link.outerHTML;
+        } else {
+            link.outerHTML = `<u>${ref}</u>`;
+        }
+    }
     // Output
     return window.document.documentElement.outerHTML;
 }
@@ -54,7 +64,7 @@ function renderBtx(this: Hexo, data: Hexo.extend.RendererData){
     const promise = _renderBtx(this, data);
     if (data.path) {
         promises[data.path] = promise;
-        console.log("Banana has: ", data.path);
+        // console.log("Banana has: ", data.path);
     }
     return promise;
 }
